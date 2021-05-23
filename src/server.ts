3e7e1a10f89ac4758ef2e78fbe20065bc278e1e1
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2021-05-23 12:17:20 +0200

    add validator and logging

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan'; // logging middleware
+import { check, validationResult } from 'express-validator'; // validation middleware
 
 const app = express();
-
 const PORT = 3001;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req: any, res: any) => res.status(200).json({ msg: 'Pino is born' }));
+
+app.get('/ueue', (req: any, res: any) => res.status(200).json({ msg: 'Pino is crying' }));
+
+app.post('/kiss', (req: any, res: any) =>
+  res.status(200).json({ msg: `Pino doesn't cry anymore` }),
+);
 
 app.listen(PORT, () => {
   // tslint:disable-next-line:no-console
   console.log(`🌲 [server]: Pino is running at http://localhost:${PORT}`);
 });
-
-app.get('/', (req: any, res: any) => res.send('Pino is born'));
-
-app.get('/ueue', (req: any, res: any) => res.send('Pino is crying'));
-
-app.post("/kiss",(req: any, res: any) => res.send('Pino doesn\'t cry anymore'))
