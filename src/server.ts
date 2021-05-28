4deeebd8cbe90102cf27a4208b6db66d417125b7
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2021-05-29 00:48:59 +0200

    CORS disabled

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import morgan from 'morgan'; // logging middleware
 import { check, validationResult } from 'express-validator'; // validation middleware
 import { checkIfAuthenticated, createUser } from './auth-middleware';
@@ -9,6 +11,11 @@ const PORT = 3001;
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors()); // cors abilitati
+
+// app.use(cors({
+//   origin: 'https://alice.policumbent.it'
+// }));
 
 app.get('/', (req: any, res: any) => res.status(200).json({ msg: 'Pino is born' }));
 
