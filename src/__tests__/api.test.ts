1330e10a566ef88454eaa05f95dbbd7e2558083e
Davide Aimar <davide.aimar99@gmail.com>
2021-12-02 14:51:14 +0100

    moved: __tests__ to src/__tests__

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../src/app';
+import app from '../app';
 
 describe('Test `/test` path', () => {
   test('It should response the GET method', () => {
